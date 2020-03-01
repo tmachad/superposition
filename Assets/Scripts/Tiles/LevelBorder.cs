963f07c42110e9aa1af6c12a57565a43f7dca714
Tom Machado <tmachad3@gmail.com>
2020-03-01 15:25:51 -0500

    Added trigger to level border tiles

---
+++ Assets/Scripts/Tiles/LevelBorder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBorder : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.Instance.GameOver();
+    }
+}
